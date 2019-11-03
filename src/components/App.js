f83bd46332a5bd54114ec3f7955dbcf256f6a0ab
Enrick <enrickdaltro@gmail.com>
2019-11-03 19:23:36 -0300

    create Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
-    return <div>WebSite</div>;
+    return <Header />;
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Header extends React.Component {
+  render() {
+    return <div>Header</div>;
+  }
+}
+
+export default Header;
