d2fdd40fd58065140998974086b8e261f1a0e261
Enrick <enrickdaltro@gmail.com>
2019-11-03 20:41:27 -0300

    add style to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import "./Header.css";
 class Header extends React.Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <ul className="header-navbar">
+        <li className="left-icon">
+          <a href="#">Enrick Daltro</a>
+        </li>
+        <li className="right-icons">
+          <li>
+            <a href="#">videos</a>
+          </li>
+          <li>
+            <a href="#">photos</a>
+          </li>
+          <li>
+            <a href="#">contact</a>
+          </li>
+        </li>
+      </ul>
+    );
   }
 }
 
