2379170823863f56333637bf64009cebbdc654cf
Enrick <enrickdaltro@gmail.com>
2019-11-03 22:40:25 -0300

    creating route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+
 class Header extends React.Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
+import Header from "./components/Header/Header";
+import Video from "./video/Video";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const Routes = () => (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Header} />
+      <Route exact path="/" component={Video} />
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
+ReactDOM.render(<Routes />, document.querySelector("#root"));
