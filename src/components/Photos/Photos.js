c6db9f615dd89906d47e24178979634930f0049c
Enrick <enrickdaltro@gmail.com>
2019-11-06 23:54:52 -0300

    set up photo component

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Photos extends Component {
   render() {
-    return <div>Photos</div>;
+    return (
+      <div>
+        <img src="" alt="" />
+      </div>
+    );
   }
 }
